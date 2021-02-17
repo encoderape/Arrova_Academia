1bc385c1befda7359653522c2e16c81197247217
algoroloro <algoro9697@gmail.com>
2021-02-17 19:36:20 +0100

    About CRUD

---
+++ backend/controllers/aboutController.js
@@ -0,0 +1,38 @@
+const about = require('../models/aboutModel.js');
+
+const controller = {
+    create: async (req, res) => {
+        try {
+            const item = await about.create(req.body);
+            res.status(201).send(item);
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    },
+    read: async (req, res) => {
+        try {
+            const items = await about.find({});
+            res.status(200).send(items);
+        } catch (err) {
+            res.status(400).send(err);
+        }
+    },
+    update: async (req, res) => {
+        try {
+            await about.findByIdAndUpdate(req.params.id, req.body);
+            res.sendStatus(201);
+        } catch (err) {
+            res.status(404).send(err);
+        }
+    },
+    delete: async (req, res) => {
+        try {
+            await about.findByIdAndDelete(req.params.id);
+            res.sendStatus(201);
+        } catch (err) {
+            res.status(404).send(err);
+        }
+    }
+}
+
+module.exports = controller;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,5 +1,7 @@
+const aboutRouter = require('./about/aboutRouter.js');
 const contactRoutes = require('./contact/contactRouter.js');
 
 module.exports = {
+    aboutRouter,
     contactRoutes,
 };
