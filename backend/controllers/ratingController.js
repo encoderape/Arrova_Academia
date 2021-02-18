217f069c3f065fc3746ee6de25010079ff9f2bf1
Ramses797 <rmorenor97@gmail.com>
2021-02-18 10:38:55 +0100

    controlador valoraciones creado

---
+++ backend/controllers/ratingController.js
@@ -0,0 +1,41 @@
+const rating = require('../models/ratingModel.js');
+
+const controller = {
+    create: async (req, res) => {
+        try{
+            let item = await rating.create(req.body);
+            res.status(201).send(item);
+        }catch(err) {
+            res.status(500).send(err);
+        }
+    },
+
+    read: async (req, res) => {
+        try{
+            let item = await rating.find({});
+            res.status(201).send(item);
+        }catch(err) {
+            res.status(500).send(err);
+        }
+    },
+
+    readByRate: async (req, res) => {
+        try{
+            let item = await rating.find(req.params.rating);
+            res.status(201).send(item);
+        }catch(err) {
+            res.status(404).send(err);
+        }
+    },
+
+    redByDate: async (req, res) => {
+        try{
+            let item = await rating.find(req.params.date);
+            res.status(201).send(item);
+        }catch(err) {
+            res.status(404).send(err);
+        }
+    }
+};
+
+module.exports = controller;
