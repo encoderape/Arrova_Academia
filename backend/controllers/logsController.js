3c42357f74488afb118ae434c4c5f6dee18310b4
Ramses797 <rmorenor97@gmail.com>
2021-02-19 10:35:37 +0100

    cambio en el controller de logs me faltaba await

--- backend/controllers/logsController.js
+++ backend/controllers/logsController.js
@@ -3,7 +3,7 @@ const logs = require('../models/logsModel.js');
 const controller = {
     create: async (req, res) => {
         try{
-            const item = logs.create(req.body);
+            const item = await logs.create(req.body);
             res.status(201).send(item);
         }catch(err) {
             res.status(400).send(err);
