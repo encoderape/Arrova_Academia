b73fa5a6864a344ea7f30cb306fe4376b153622b
Ramses797 <rmorenor97@gmail.com>
2021-02-19 10:00:30 +0100

    modelo logs creado

---
+++ backend/models/logsModel.js
@@ -0,0 +1,29 @@
+const mongoose  = require('mongoose');
+
+const schema = mongoose.Schema(
+    {
+        message: {
+            type: String,
+            required: true
+        },
+        additional: {
+            type: [String]
+        },
+        level: {
+            type: Number,
+            required: true
+        },
+        fileName: {
+            type: String,
+            required: true
+        },
+        lineNumber: {
+            type: String,
+            required: true
+        }
+    },
+    {timestamps: true}
+);
+
+const model = mongoose.model('Logs', schema);
+module.exports = model;
