51f48bbd6b4c79c8a1b26bec97b59b91f18a8174
algoroloro <algoro9697@gmail.com>
2021-02-19 20:18:09 +0100

    User Auth Middleware

--- backend/routes/user/userRouter.js
+++ backend/routes/user/userRouter.js
@@ -1,14 +1,15 @@
 const method = require('../../controllers/userController.js');
+const isUserAuth = require('../../middlewares/user-auth.js');
 
 const router = express => {
     const api = express.Router();
     api.post('/create', method.create);
     api.post('/login', method.login);
-    api.post('/reset/:token', method.createNewPassword);
+    api.post('/reset/:token', isUserAuth, method.createNewPassword);
     api.get('/read', method.read);
-    api.get('/read/:id', method.readById);
-    api.get('/reset', method.resetPassword);
-    api.put('/update/:id', method.update);
+    api.get('/read/:id', isUserAuth, method.readById);
+    api.get('/reset', isUserAuth, method.resetPassword);
+    api.put('/update/:id', isUserAuth, method.update);
     api.delete('/delete/:id', method.delete);
     return api;
 }
