f3afbaa68a7aa9713b2a35b3ccfa1b48031b9f31
Ramses797 <rmorenor97@gmail.com>
2021-02-20 10:28:56 +0100

    modelo encValenciano creado

---
+++ backend/models/encValencianModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            enum: ['a2', 'b1', 'b2', 'c1', 'c2'],
+            required: true
+        },
+        nativeLenguage: {
+            type: String,
+            enum: ['valenciano', 'castellano'],
+            required: true
+        },
+        previousTitle: {
+            type: Boolean,
+            required: true
+        },
+        studies: {
+            type: String,
+            enum: ['eso', 'bach', 'cgm', 'cgs', 'superiores'],
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+            //TODO: REGEX
+        },
+        terms: {
+            type: Boolean,
+            default: false
+        },
+        comment: {
+            type: String,
+            maxlength: 500,
+            required: true
+        }
+    }
+);
+
+const model = mongoose.model('EcnValencian', schema);
+module.exports = model;
