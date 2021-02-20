fcd12842fc51cf9ecbddef8fcda28e2524ab1edb
Ramses797 <rmorenor97@gmail.com>
2021-02-20 12:05:42 +0100

    CRUD enctitulaciones creado

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ app.use(cors);
 endpointConfig(app, '/about', endpoints.aboutRoutes);
 endpointConfig(app, '/contact', endpoints.contactRoutes);
 endpointConfig(app, '/ecnProfessional', endpoints.ecnProfessionalRoutes);
+endpointConfig(app, '/encTitulaciones', endpoints.ecnTitulacionesRoutes);
 endpointConfig(app, '/ecnValenciano', endpoints.ecnValencianoRoutes);
 endpointConfig(app, '/logs', endpoints.logsRoutes);
 endpointConfig(app, '/news', endpoints.newsRoutes);
