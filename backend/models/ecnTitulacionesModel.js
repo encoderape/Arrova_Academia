0a381446c780c753d30b3de2dbe362fff2949c08
Ramses797 <rmorenor97@gmail.com>
2021-02-20 11:55:11 +0100

    modelo ecntitulaciones creado

---
+++ backend/models/ecnTitulacionesModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const ms = require('ms');
+
+const schema = new mongooseSchema(
+    {
+        title: {
+            type: String,
+            enum: ['eso', 'bach'],
+            required: true
+        },
+        nativeLenguage: {
+            type: String,
+            enum: ['valenciano', 'castellano'],
+            required: true
+        },
+        englishLevel: {
+            type: String,
+            enum: ['a2', 'b1', 'b2', 'c1', 'c2'],
+            required: true
+        },
+        studies: {
+            type: String,
+            enum: ['eso', 'egb', 'bup', 'bach', 'cou'],
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            //TODO: REGEX
+        },
+        terms: {
+            type: Boolean,
+            default: false
+        },
+        comment: {
+            type: String,
+            maxlength: 500,
+            required: true
+        },
+        expireAt: {
+            type: Date,
+            default: Date.now,
+            index: {
+                expires: ms('30 days'),
+            },
+        },
+    },
+    {timestamps: true}
+);
+
+const model = mongoose.model('EcnTitulaciones', schema);
+module.exports = model;
