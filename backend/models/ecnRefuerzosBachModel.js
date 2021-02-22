380f80e4d78f00031168a670a83e97f765331efa
Ramses797 <rmorenor97@gmail.com>
2021-02-22 10:33:44 +0100

    modelo CRUD encrefuerzos bach creado

---
+++ backend/models/ecnRefuerzosBachModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const ms = require('ms');
+
+const schema = new mongoose.Schema(
+    {
+        letters: {
+            type: String,
+            enum: ['val', 'cas', 'latin', 'griego', 'ingles', 'filo', 'geo', 'hist', 'hist arte', 'hist esp']
+        },
+        sciences: {
+            type: String,
+            enum: ['mates', 'mates aplicadas', 'fisica', 'quimica', 'bio', 'geo']
+        },
+        email: {
+            type: String,
+            required: true,
+            //TODO: REGEX
+        },
+        terms: {
+            type: Boolean,
+            default: false
+        },
+        comment: {
+            type: String,
+            required: true
+        },
+        expireAt: {
+            type: Date,
+            default: Date.now,
+            index: {
+                expires: ms('30 days'),
+            },
+        },
+    },
+    {timestamps: true}
+);
+
+const model = mongoose.model('EcnRefuerzosBach', schema);
+module.exports = model;
