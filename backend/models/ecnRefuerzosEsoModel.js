fe03f31e586bf4fd619487b293794dc1d641c1cc
Ramses797 <rmorenor97@gmail.com>
2021-02-22 09:41:24 +0100

    modelo y controlador del CRUDRefuerzoEso creados

---
+++ backend/models/ecnRefuerzosEsoModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const ms = require('ms');
+const schema = new mongoose.Schema(
+    {
+        letters: {
+            type: String,
+            enum: ['val', 'cas', 'latin', 'ingles', 'geo', 'historia'],
+        },
+        sciences: {
+            type: String,
+            enum: ['mates', 'fiq', 'bio', 'geo']
+        },
+        email: {
+            type: String,
+            required: true,
+            //TODO: REGEX
+        },
+        terms: {
+            type: Boolean,
+            default: false
+        },
+        comment: {
+            type: String,
+            required: true
+        },
+        expireAt: {
+            type: Date,
+            default: Date.now,
+            index: {
+                expires: ms('30 days'),
+            },
+        },
+    },
+    {timestamps: true}
+);
+
+const model = mongoose.model('EcnRefuerzosEso', schema);
+module.exports = model;
