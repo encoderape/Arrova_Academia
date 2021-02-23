b471568a6b41b700afff554e4e72829ecc47e7b5
algoroloro <algoro9697@gmail.com>
2021-02-23 19:45:40 +0100

    Phone number Validator Unit Test

--- backend/tests/unit/onlyStringValidator.test.js
+++ backend/tests/unit/onlyStringValidator.test.js
@@ -1,11 +1,11 @@
 const onlyStringValidator = require('../../validators/onlyStringValidator.js')
 
 describe('only strings validation', () => {
-    test('this is an only string', () => {
+    it('this is an only string', () => {
         const string = 'hi world';
         expect(true).toBe(onlyStringValidator(string));
     });
-    test('this is not an only string', () => {
+    it('this is not an only string', () => {
         const string = 'hi w0rld';
         expect(false).toBe(onlyStringValidator(string));
     });
