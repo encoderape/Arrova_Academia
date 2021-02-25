0fd387dcb0ff3af8800b65d5606fc756304751aa
algoroloro <algoro9697@gmail.com>
2021-02-25 22:49:41 +0100

    refuerzos uni pdf sending successfully

--- backend/controllers/ecnRefuerzosUniController.js
+++ backend/controllers/ecnRefuerzosUniController.js
@@ -1,10 +1,16 @@
 const refuerzosUni = require('../models/ecnRefuerzosUniModel.js');
+const refuerzosUniPdf = require('../mail/refuerzosUniPdf.js');
+const sendMail = require('../mail/sendMail.js');
 
 const controller = {
     create: async (req, res) => {
         try{
             const item = await refuerzosUni.create(req.body);
-            res.status(201).send(item);
+            await refuerzosUniPdf(item);
+            setTimeout(() => {
+                sendMail(item.email, `Estudia con nosotros Refuerzos Uni ${item.fullName}`, 'refuerzosuni.pdf');
+                res.status(201).send(item);
+            }, 2000);
         }catch(err) {
             res.status(400).send(err);
         }
