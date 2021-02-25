7a12c490bb6c356092392e6889f13c71a87f989e
algoroloro <algoro9697@gmail.com>
2021-02-25 18:30:08 +0100

    create pdf util

---
+++ backend/utils/createPdf.js
@@ -0,0 +1,18 @@
+const pdf = require('html-pdf');
+const path = require('path');
+
+const rootDir = require('./path.js');
+
+const options = {
+    format: 'A4',
+    orientation: 'portrait',
+    type: 'pdf'
+}
+
+function createPdf(content, fileName) {
+    pdf.create(content, options).toFile(path.join(rootDir, 'files', fileName), () => {
+        console.log('pdf created successfully');
+    });
+}
+
+module.exports = createPdf;
