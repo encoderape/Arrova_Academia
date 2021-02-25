e1f7881a274bae6683b1994f24be3c0fbbc0011b
algoroloro <algoro9697@gmail.com>
2021-02-25 23:01:34 +0100

    valenciano pdf sending successfully

--- backend/controllers/ecnValencianoController.js
+++ backend/controllers/ecnValencianoController.js
@@ -1,10 +1,16 @@
 const valenciano = require('../models/encValencianoModel.js');
+const valencianoPdf = require('../mail/valencianoPdf.js');
+const sendMail = require('../mail/sendMail.js');
 
 const controller = {
     create:  async (req, res) => {
         try{
             const item = await valenciano.create(req.body);
-            res.status(201).send(item);
+            await valencianoPdf(item);
+            setTimeout(() => {
+                sendMail(item.email, `Estudia con nosotros Valenciano ${item.fullName}`, 'valenciano.pdf');
+                res.status(201).send(item);
+            }, 2000);
         }catch(err) {
             res.status(400).send(err);
         }
