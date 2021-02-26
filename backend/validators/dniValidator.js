f21ac4233bd05dcacfabff75d7cfe394040349a9
algoroloro <algoro9697@gmail.com>
2021-02-26 17:58:53 +0100

    Dni complete validator

--- backend/validators/dniValidator.js
+++ backend/validators/dniValidator.js
@@ -1,5 +1,10 @@
 function dniValidator(dni) {
-    return /^[0-9]{8}[A-Z]$/.test(dni);
+    if (/^[0-9]{8}[A-Z]$/.test(dni)) {
+        const dni_letters = "TRWAGMYFPDXBNJZSQVHLCKE";
+        const letter = dni_letters.charAt(parseInt(dni, 10) % 23);
+        return letter === dni.charAt(8);
+    }
+    return false;
 }
 
 module.exports = dniValidator;
